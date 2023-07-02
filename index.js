0883958d594948c19c646e36bc85c689b920438c
vincent <innocent0705@gmail.com>
2023-07-03 00:47:10 +0800

    add api console.log

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(express.json());
 app.use(
     cors({
-      origin: "http://localhost:5000/",
+      origin: "http://localhost:5000",
       methods: "GET,POST,PUT,DELETE",
       credentials: true,
     })
