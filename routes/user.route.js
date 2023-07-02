06259c9a36b6395858507a7bbe61a9a1890abe67
vincent <innocent0705@gmail.com>
2023-07-02 22:13:12 +0800

    update router

--- routes/user.route.js
+++ routes/user.route.js
@@ -19,11 +19,15 @@ router.get("/login/success", (req, res) => {
 });
 
 router.get("/current_user", (req, res) => {
-    res.status(200).json({
-        error: false,
+    if (req.user) {
+		res.status(200).json({
+			error: false,
 			message: "Successfully Loged In",
 			user: req.user,
 		});
+	} else {
+		res.status(403).json({ error: true, message: "Not Authorized" });
+	}
   });
 
 router.post("/update", update);
