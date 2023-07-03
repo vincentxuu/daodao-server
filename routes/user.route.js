a4b61f5c8e3c567d07488721a932a655c023aea9
vincent <innocent0705@gmail.com>
2023-07-03 14:36:45 +0800

    update current_user controller

--- routes/user.route.js
+++ routes/user.route.js
@@ -18,20 +18,21 @@ router.get("/login/success", (req, res) => {
 	}
 });
 
-router.get("/current_user", (req, res) => {
-	console.log("/current_user.req",req)
-	console.log("req.session:",req.session)
-    if (req.user) {
-		res.status(200).json({
-			error: false,
-			message: "Successfully Loged In",
-			user: req.user,
-		});
-	} else {
-		res.status(403).json({ error: true, message: "Not Authorized" });
-	}
-  });
+// router.get("/current_user", (req, res) => {
+// 	console.log("/current_user.req",req)
+// 	console.log("req.session:",req.session)
+//     if (req.user) {
+// 		res.status(200).json({
+// 			error: false,
+// 			message: "Successfully Loged In",
+// 			user: req.user,
+// 		});
+// 	} else {
+// 		res.status(403).json({ error: true, message: "Not Authorized" });
+// 	}
+//   });
 
+router.get("/current_user",profile);
 router.post("/update", update);
 
 router.get("/all_User", getAllUser);
