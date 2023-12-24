6c1ac32ae7382f7c4ede7060e30cff9fe42195ac
vincentxu777 <vincentxu@pershing.com.tw>
2023-12-24 17:10:18 +0800

    update cors

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(express.json());
 app.use(
     cors({
-      origin: [process.env.FRONTEND_URL,'https://daodao-f2e-daodaoedu.vercel.app','https://daodao-f2e-pi.vercel.app'],
+      origin: [process.env.FRONTEND_URL,'https://daodao-f2e-daodaoedu.vercel.app','https://daodao-f2e-pi.vercel.app','http://localhost:5000'],
       methods: "GET,POST,PUT,DELETE",
       credentials: true,
     })
